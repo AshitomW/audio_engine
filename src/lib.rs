36ae972aa44a80deddb6a013cca4b8f87235f299
Ashitom <sasukeuchia32828@gmail.com>
2026-02-07 10:47:55 +0545

    Init

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! Real Time Audio Processing Engine In Rust
+
+#![forbid(unsafe_code)]
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![deny(clippy::cast_possible_truncation)]
+#![deny(clippy::cast_sign_loss)]
+#![deny(clippy::cast_precision_loss)]
+#![deny(clippy::cast_possible_wrap)]
+#![allow(clippy::module_name_repetitions)]
+
+pub mod error;
